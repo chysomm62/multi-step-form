175675e5a3e0750e417fdf69ebe060aad6eb1ce2
chysomm62 <muorahmarypeace@gmail.com>
2023-05-03 12:28:26 +0100

    complete first step

--- src/components/Form/SecondStep.jsx
+++ src/components/Form/SecondStep.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./Form.module.scss";
 
 const plans = [
@@ -19,13 +20,24 @@ const plans = [
 ];
 
 const SecondStep = () => {
+  const [planIndex, setPlanIndex] = useState(0);
+  const [isToggled, setIsToggled] = useState(false);
+
   return (
     <>
       <h1 className={styles.h1}>Select your plan</h1>
       <p> You have the option of monthly or yearly billing.</p>
       <div className={styles.plans}>
-        {plans.map((plan) => (
-          <div className={styles.plan} key={plan.name}>
+        {plans.map((plan, index) => (
+          <div
+            className={
+              planIndex === index
+                ? `${styles.plan} ${styles.selected}`
+                : styles.plan
+            }
+            key={plan.name}
+            onClick={() => setPlanIndex(index)}
+          >
             <img src={plan.icon} alt={plan.name} />
             <div>
               <h3>{plan.name}</h3>
@@ -35,8 +47,26 @@ const SecondStep = () => {
           </div>
         ))}
       </div>
+      <Switch isToggled={isToggled} setIsToggled={setIsToggled} />
     </>
   );
 };
 
 export default SecondStep;
+
+const Switch = ({ isToggled, setIsToggled }) => {
+  return (
+    <div className={styles.switchWrap}>
+      <p className={isToggled ? styles.gray_p : styles.blue_p}>Monthly</p>
+      <label className={styles.switch}>
+        <input
+          type="checkbox"
+          checked={isToggled}
+          onChange={() => setIsToggled(!isToggled)}
+        />
+        <span className={styles.slider} />
+      </label>
+      <p className={!isToggled ? styles.gray_p : styles.blue_p}>Yearly</p>
+    </div>
+  );
+};
--- src/components/Form/ThirdStep.jsx
+++ src/components/Form/ThirdStep.jsx
@@ -1,10 +1,55 @@
+import { useState } from "react";
 import styles from "./Form.module.scss";
 
+const addOns = [
+  {
+    name: "Online service",
+    detail: " Access to multiplayer games",
+    price: "+$1/mo",
+  },
+  {
+    name: "Larger storage",
+    detail: " Extra 1TB of cloud save",
+    price: "+$2/mo",
+  },
+  {
+    name: "Customizable Profile",
+    detail: "Custom theme on your profile",
+    price: "+$2/mo",
+  },
+];
+
 const ThirdStep = () => {
+  const [isToggled, setIsToggled] = useState(false);
+  const [selected, setSelected] = useState([]);
+
   return (
     <>
       <h1 className={styles.h1}>Pick add-ons</h1>
       <p> Add-ons help enhance your gaming experience.</p>
+      <div className={styles.addOns}>
+        {addOns.map((addOn, index) => (
+          <div key={addOn.name} className={styles.addOn}>
+            <div className={styles.first_child}>
+              <label className={styles.checkbox}>
+                <input
+                  type="checkbox"
+                  // checked={isToggled}
+                  // onChange={() => setIsToggled(!isToggled)}
+                />
+                <span className={styles.checkmark} />
+              </label>
+              <div>
+                <h3>{addOn.name}</h3>
+                <p>{addOn.detail}</p>
+              </div>
+            </div>
+            <div>
+              <h4 className={styles.price}>{addOn.price}</h4>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
